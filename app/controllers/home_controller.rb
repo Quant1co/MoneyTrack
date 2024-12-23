cf7537c09dde00e013ee5a72dcaab5621a5012f2
lok70 <64475032+lok70@users.noreply.github.com>
2024-12-23 17:48:59 +0300

    fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,13 +4,13 @@ class HomeController < ApplicationController
   def index
 
     if current_user
-      redirect_to logined_path
+      session[:user_id] = nil
     end
 
-    advice_data = DataParserJob.new('https://journal.tinkoff.ru/flows/fin-advice/')
-    advice_data.parse_advices
-    advice = ParserToDb.new(advice_data.advices)
-    advice.save_to_db
+    # advice_data = DataParserJob.new('https://journal.tinkoff.ru/flows/fin-advice/')
+    # advice_data.parse_advices
+    # advice = ParserToDb.new(advice_data.advices)
+    # advice.save_to_db
     $arr_advices = Advice.all
 
 
@@ -18,7 +18,7 @@ class HomeController < ApplicationController
     service = CurrencyService.new(api_key)
 
     # Получаем курсы валют относительно USD
-    $rates = service.get_exchange_rates
+    #$rates = service.get_exchange_rates
 
   end
 
