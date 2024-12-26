9bdc39b6c2c1a260b54be4d5121345d31b5d51fb
lok70 <64475032+lok70@users.noreply.github.com>
2024-12-26 17:42:37 +0300

    fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,10 +7,10 @@ class HomeController < ApplicationController
       session[:user_id] = nil
     end
 
-    # advice_data = DataParserJob.new('https://journal.tinkoff.ru/flows/fin-advice/')
-    # advice_data.parse_advices
-    # advice = ParserToDb.new(advice_data.advices)
-    # advice.save_to_db
+    advice_data = DataParserJob.new('https://journal.tinkoff.ru/flows/fin-advice/')
+    advice_data.parse_advices
+    advice = ParserToDb.new(advice_data.advices)
+    advice.save_to_db
     $arr_advices = Advice.all
 
 
@@ -18,7 +18,7 @@ class HomeController < ApplicationController
     service = CurrencyService.new(api_key)
 
     # Получаем курсы валют относительно USD
-    #$rates = service.get_exchange_rates
+    $rates = service.get_exchange_rates
 
   end
 
--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -6,15 +6,14 @@ class StocksController < ApplicationController
     service = StockPriceService.new(api_key)
 
     #Получаем тикеры популярных акций
-    # @tickers = service.get_popular_tickers(5)
+    @tickers = service.get_popular_tickers(5)
 
     #tickers = ['AAPL', 'MSFT', 'AMZN', 'TSLA', 'GOOG', 'META', 'NVDA', 'BRK.A', 'JNJ', 'JPM', 'V', 'PG', 'WMT', 'HD', 'KO']
 
-    # $stocks = @tickers.map do |ticker|
-    #   price = service.fetch_price(ticker)
-    #   { ticker: ticker, price: price } if price
-    # end.compact
-    # Предполагается, что $stocks — это массив хэшей с ключами :ticker и :price
+    $stocks = @tickers.map do |ticker|
+      price = service.fetch_price(ticker)
+      { ticker: ticker, price: price } if price
+    end.compact
     render json: $stocks
   end
 end
