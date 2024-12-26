471ea96bae7505a0f82285c8d6ded51388f96f8b
lok70 <64475032+lok70@users.noreply.github.com>
2024-12-26 18:26:06 +0300

    FIX DEL FROM FAV

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
 
   get 'logined', to: 'logined#index', as: 'logined'
   post 'logined', to: 'logined#create'
+  delete 'logined', to: 'logined#destroy'
 
   get 'login', to: 'sessions#new', as: 'login'
   post 'login', to: 'sessions#create'
